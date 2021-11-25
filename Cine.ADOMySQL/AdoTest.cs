aee45a70cfa2305957a6edbfc8855455602cca09
alex171615 <92887488+alex171615@users.noreply.github.com>
2021-11-25 15:17:51 -0300

    agrgue nuevos insert

---
+++ Cine.ADOMySQL/AdoTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using et12.edu.ar.AGBD.Ado;
+
+namespace AGBD.Test
+{
+    public class AdoTest
+    {
+        public AdoAGBD Ado { get; set; }
+        public MapCliente MapCliente { get; set; }
+        public MapEntrada MapEntrada { get; set; }
+        public AdoTest(AdoAGBD ado)
+        {
+            Ado = ado;
+            MapCliente = new MapCliente(Ado);
+            MapEntrada = new MapEntrada(MapCliente);
+        }
+        public void AltaCliente(Cliente cliente) => MapCliente.AltaCliente(cliente);
+        public List<Cliente> ObtenerClientes() => MapCliente.ObtenerClientes();
+        public List<Entrada> obtenerEntradas() => MapEntrada.obtenerEntradas();
+        public List<Entrada> obtenerEntradas(Cliente cliente)
+            => MapProducto.obtenerEntradas(cliente);
+        public void AltaEntrada(Entrada entrada) => MapEntrada.AltaEntrada(entrada);
+
+    }
+}
--- Scripts SQL/03 Insert.sql
+++ Scripts SQL/03 Insert.sql
@@ -2,16 +2,16 @@ use cine;
 START TRANSACTION;
 
 insert into cliente (idcliente, nombre, apellido, email, contrasena)
-values (0001,"Alexis", "Noguera", "AlexisNoguera@gmail.com", sha2(contrasena,256));
-
-insert into entrada (idEntrada, idCliente, valor, email, numeroEntrada, idProyeccion)
-values (0002, 0023, 350, "Alexis@gmail.com", 1234, 0003);
-
-insert into proyeccion (idProyeccion, fechaHora, idPelicula, idSala, idCliente)
-values (0008, "2021-07-14 22:30" , 0004, 0005, 0022); 
+values (1,"Alexis", "Noguera", "AlexisNoguera@gmail.com", sha2(contrasena,256));
 
 insert into pelicula (idPelicula, nombre, fecha, genero)
-values ( 0004, "It2", "2021-07-21", "Terror"); 
+values ( 4, "It2", "2021-07-21", "Terror");  
+
+insert into proyeccion (idProyeccion, fechaHora, idPelicula, idSala, idCliente)
+values (9, "2021-07-14 22:30" , 4, 9, 1); 
 
 insert into sala (idSala, piso, capacidad)
-values (0009, 2, 500); 
+values (9, 2, 500);
+
+insert into entrada (idEntrada, valor, email, numeroEntrada, idProyeccion, idCliente)
+values (2, 350, "AlexisNoguera@gmail.com", 1234, 9, 1);
