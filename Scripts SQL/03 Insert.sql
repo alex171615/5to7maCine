726326a97e3745750f7d5e17c2de196376aa9060
alex171615 <92887488+alex171615@users.noreply.github.com>
2021-12-01 18:09:26 -0300

    mejore el insert
    a

--- Scripts SQL/03 Insert.sql
+++ Scripts SQL/03 Insert.sql
@@ -7,11 +7,11 @@ values (1,"Alexis", "Noguera", "AlexisNoguera@gmail.com", sha2(contrasena,256));
 insert into pelicula (idPelicula, nombre, fecha, genero)
 values ( 4, "It2", "2021-07-21", "Terror");  
 
-insert into proyeccion (idProyeccion, fechaHora, idPelicula, idSala, idCliente)
-values (9, "2021-07-14 22:30" , 4, 9, 1); 
-
 insert into sala (idSala, piso, capacidad)
-values (9, 2, 500);
+values (12, 2, 500);
 
-insert into entrada (idEntrada, valor, email, numeroEntrada, idProyeccion, idCliente)
-values (2, 350, "AlexisNoguera@gmail.com", 1234, 9, 1);
+insert into proyeccion (idProyeccion, fechaHora, idPelicula, idSala, idCliente)
+values (9, "2021-07-14 22:30", 4, 12, 1); 
+
+insert into entrada (idEntrada, valor, numeroEntrada, idProyeccion)
+values (2, 350, 1234, 9);
